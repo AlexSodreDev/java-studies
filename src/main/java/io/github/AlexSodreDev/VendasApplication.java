39fc3b0bcc816b889c48c87d5e50978cc977e791
AlexSodreDev <alexandre.cortines@gmail.com>
2023-12-19 22:51:21 -0300

    Realizando as operações do CRUD atraves de JdbTemplate

--- src/main/java/io/github/AlexSodreDev/VendasApplication.java
+++ src/main/java/io/github/AlexSodreDev/VendasApplication.java
@@ -16,11 +16,36 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
+            System.out.println("Salvando Clientes");
             clientes.salvar(new Cliente("Cliente 1"));
             clientes.salvar(new Cliente("Cliente 2"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando Clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + ", atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando Clientes por Nome");
+            clientes.buscaPorNome("2").forEach(System.out::println);
+
+//            System.out.println("Deletando Clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum Cliente encontrado");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
 
--- src/main/java/io/github/AlexSodreDev/domain/repository/Clientes.java
+++ src/main/java/io/github/AlexSodreDev/domain/repository/Clientes.java
@@ -3,8 +3,11 @@ package io.github.AlexSodreDev.domain.repository;
 import io.github.AlexSodreDev.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -12,6 +15,8 @@ public class Clientes {
 
     private static final String INSERT = "insert into cliente (nome) values (?)";
     private static final String SELECT_ALL = "select * from cliente";
+    private static final String UPDATE = "update cliente set nome = (?) where id = (?)";
+    private static final String DELETE = "delete from cliente where id = (?)";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -21,11 +26,39 @@ public class Clientes {
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscaPorNome(String nome) {
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like (?) "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, (resultSet, i) -> {
-            Integer id = resultSet.getInt("id");
-            String nome = resultSet.getString("nome");
-            return new Cliente(id, nome);
-        });
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private static RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id, nome);
+            }
+        };
     }
 }
